2662beaec99b53b30fb1890f95c214a3750b94de
André Passos <52414263+andregpassos@users.noreply.github.com>
2019-10-31 14:46:39 -0300

    Add files via upload

---
+++ heapsort.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tamanho_heap;
+
+void max_heapify(int arvore[], int i) {
+    int l, r, maior, temp;
+
+    l = 2*i; //ESQUERDO(i)
+    r = 2*i + 1; //DIREITO(i)
+
+    //se o filho da esquerda for maior, guarda sua posicao no maior.
+    if (l <= tamanho_heap && arvore[l] > arvore[i])
+        maior = l;
+    else //se nao, a posicao do maior eh o proprio i.
+        maior = i;
+    //se o filho da direita for maior, guarda sua posicao no maior.
+    if (r <= tamanho_heap && arvore[r] > arvore[maior])
+        maior = r;
+
+    //se o maior nao for o proprio i, troca arvore[i] <-> arvore[maior] e executa o recursivo.
+    if (maior != i) {
+        temp = arvore[i];
+        arvore[i] = arvore[maior];
+        arvore[maior] = temp;
+
+        max_heapify(arvore, maior);
+    }
+}
+
+void build_max_heap(int arvore[]){
+    for (int i = tamanho_heap/2; i >= 1; i--)
+        max_heapify(arvore, i);
+}
+
+void heap_sort(int arvore[]) {
+    int temp, comprimento = tamanho_heap;
+
+    build_max_heap(arvore);
+    for (int i = comprimento; i > 1; i--) {
+        temp = arvore[i];
+        arvore[i] = arvore[1];
+        arvore[1] = temp;
+
+        tamanho_heap = tamanho_heap -1;
+        max_heapify(arvore, 1);
+    }
+}
+
+int main() {
+    int arvore[] = {6,8,9,7,16,10,14};
+    //posicao 0 do vetor indica o tamanho do heap.
+    //heap binario comeca da posicao 1.
+    tamanho_heap = arvore[0];
+
+    heap_sort(arvore);
+
+    printf("Heap binario ordenado por heapsort:\n\n");
+
+    for (int i = 1; i <= arvore[0]; i++)
+        printf("A[%d] = %d\n", i, arvore[i]);
+
+    return 0;
+}
