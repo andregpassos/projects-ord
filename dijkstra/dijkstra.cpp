662ecd8fd2f99558370b02fd9a3d7407edc9620d
André Passos <52414263+andregpassos@users.noreply.github.com>
2019-12-12 12:58:51 -0300

    Add files via upload

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -4,35 +4,43 @@
 #include <stdlib.h>
 using namespace std;
 
-int findMinVert(int *distancia, bool *visitado, int n) {
-    //implementar com prioridade minima
-
+//retorna o menor vertice atual (se ele nao foi visitado)
+int findMinVert(int* distancia, bool* visitado, int n) {
     int minVert = -1;
+    //verifica se o próximo vertice é menor que o menor vértice atual
     for (int i = 0; i < n; i++) {
-        if(!visitado[i] && (minVert = -1 || distancia[i] < distancia[minVert])) {
+        if(!visitado[i] && (minVert == -1 || distancia[i] < distancia[minVert])) {
             minVert = i;
         }
     }
     return minVert;
 }
 
-void dijkstra (int **matriz, int n) {
-    int *distancia = new int[n];
-    bool *visitado = new bool[n];
+void dijkstra(int** arestas, int n){
+    int* distancia = new int[n];
+    bool* visitado = new bool[n];
 
-    distancia[0] = 0;
-    visitado[0] = false;
-    for (int i = 1; i < n; i++) {
+    //INITIALIZE-SINGLE-SOURCE(G,s)
+    //Inicializando as distancias (pos[0] = 0, demais posicoes = inf)
+    for (int i = 0; i < n; i++) {
         distancia[i] = INT_MAX;
         visitado[i] = false;
     }
+    distancia[0] = 0;
 
-    for (int i = 0; i < n-1; i++) {
+    //RELAX(u,v,w)
+    //atribuindo a menor distancia a cada vertice
+    for (int i = 0; i < n - 1; i++) {
         int minVert = findMinVert(distancia, visitado, n);
+        //vertice atual recebe a flag de 'visitado'
         visitado[minVert] = true;
         for (int j = 0; j < n; j++) {
-            if (matriz[minVert][j] != 0 && !visitado[j]) {
-                int dist = distancia[minVert] + matriz[minVert][j];
+            //percorre toda os vertices da matriz
+            if (arestas[minVert][j] != 0 && !visitado[j]) {
+                //'dist' vai somando os caminhos
+                int dist = distancia[minVert] + arestas[minVert][j];
+                //se o caminho atual (soma dos caminhos) = 'dist'
+                //for menor que a distancia atual do vertice distancia[j] <-> dist
                 if (dist < distancia[j]) {
                     distancia[j] = dist;
                 }
@@ -40,7 +48,7 @@ void dijkstra (int **matriz, int n) {
         }
     }
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cout << i << " " << distancia[i] << endl;
     }
     delete [] visitado;
@@ -49,11 +57,14 @@ void dijkstra (int **matriz, int n) {
 }
 
 int main() {
-    int n;
-    char caminho[20], buffer[10];
+    int n, a;
+    char caminho[20];
 
+    cout << "\t\tALGORITMO DE DIJKSTRA\n\n";
+    cout << "Nome do arquivo> ";
     cin >> caminho;
 
+    ////////////////////////LEITURA DE ARQUIVO///////////////////////////
     ifstream txtFile;
     txtFile.open(caminho);
 
@@ -65,15 +76,14 @@ int main() {
     txtFile >> n; //guardando o primeiro valor do arquivo em 'n'
 
     //calculo do numero de arestas 'a'
-    int a = (n-1)*n*0.5;
+    a = (n-1)*n*0.5;
 
     //criando matriz n x n zerada (n qualquer)
-    int **matriz = new int *[n];
+    int** arestas = new int*[n];
     for (int i = 0; i < n; i++) {
-        matriz[i] = new int [n];
-
-        for (int j = 0; j < n; j ++) {
-                matriz[i][j] = 0;
+        arestas[i] = new int[n];
+        for (int j = 0; j < n; j++) {
+            arestas[i][j] = 0;
         }
     }
 
@@ -83,27 +93,34 @@ int main() {
     for (int i = 0; i < a; i++) {
         txtFile >> peso[i];
     }
-
     txtFile.close();
+    //////////////////////////////////////////////////////////////////
 
     //atribuindo os pesos a matriz de adjacencia
     int pos = 0;
     for (int i = 0; i < a; i++) {
         for (int j = i+1; j < n; j++) {
-            matriz[i][j] = peso[pos];
-            matriz[j][i] = peso[pos];
+            arestas[i][j] = peso[pos];
+            arestas[j][i] = peso[pos];
             pos++;
         }
     }
 
+    /*//TESTE: imprimindo matriz de adjacencia
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cout << matriz[i][j] << " ";
+            cout << arestas[i][j] << " ";
         }
         cout << endl;
-    }
-    //dijkstra(matriz, n);
+    }*/
 
+    cout << endl << "Distancias" << endl;
+    dijkstra(arestas, n);
+
+    for (int i = 0; i < n; i++) {
+        delete [] arestas[i];
+    }
+    delete [] arestas;
 
     return 0;
 }
